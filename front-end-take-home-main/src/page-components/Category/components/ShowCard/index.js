085dc479dcbaf4002b114223ddceb772857cadb4
purvi.pandya <purvi.pandya@service.nsw.gov.au>
2021-09-19 09:56:43 +1000

    added proptypes and render component

--- front-end-take-home-main/src/page-components/Category/components/ShowCard/index.js
+++ front-end-take-home-main/src/page-components/Category/components/ShowCard/index.js
@@ -1,10 +1,36 @@
 import React from 'react';
-
+import PropTypes from 'prop-types';
 function ShowCard({ shows }) {
 	return (
 		<>
 			{console.log('shows', shows)}
+			<div>
+				{shows && shows.map(show => (
+					<div key={show.id}>
+              <img src={show.images.squareLarge.url} alt="images" />
+              <p>{show.name}</p>
+              <p>{show.description}</p>
+            </div>
+				))}
+			</div>
 			</>
 	)
-}
+};
+
+ShowCard.propTypes = {
+  shows: PropTypes.arrayOf(PropTypes.shape({
+    name: PropTypes.string,
+    description: PropTypes.string,
+    images: PropTypes.shape({
+      squareLarge: PropTypes.shape({
+        url: PropTypes.string,
+      }),
+    }),
+  }))
+};
+
+ShowCard.defaultProps = {
+  shows: []
+};
+
 export default ShowCard;

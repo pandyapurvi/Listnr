a207f48c4e7639b6e254f9b1b0c55cb0ec810d18
purvi.pandya <purvi.pandya@service.nsw.gov.au>
2021-09-19 10:32:12 +1000

    tuncated header and description lines and added podcast text in header

--- front-end-take-home-main/src/page-components/Category/components/ShowCard/index.js
+++ front-end-take-home-main/src/page-components/Category/components/ShowCard/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from '../../../../css/styles.module.css';
+import Header from 'shared-components/Typography/Header';
 
 function ShowCard({ shows }) {
+
+	const podcastCount = shows && shows.length >= 1 ?
+		`${shows.length} Podcasts` :
+		`${shows.length} Podcast`;
+
 	return (
 		<>
+			<Header as="h1" variant="m" text={podcastCount} linesToShow={1} mb="m" />
 			{console.log('shows', shows)}
 			<div className={styles.container}>
 				{shows && shows.map(show => (
 					<div key={show.id}>
               <img src={show.images.squareLarge.url} alt="images" className={styles.image} />
-              <p>{show.name}</p>
-              <p>{show.description}</p>
+              <p className={styles.headerText}>{show.name}</p>
+              <p className={styles.description}>{show.description}</p>
             </div>
 				))}
 			</div>
